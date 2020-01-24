c8ac3758b95b16ea79335c343fa4396d618a0bbd
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-01-24 10:50:50 -0500

    got question to appear line 24

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,47 +8,67 @@ $(document).ready(function() {
 
     //The array of questions for out game
     var questions = [
-        { q: "Which house was Harry Potter sorted into?", a: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]},
-        { q: "What is the name of the Harry's Wizard School?", a: ["Hogwarts", "Beauxbatons", "Ilvermorny", "Durmstrang"]},
-        { q: "Who is Harry's godfather?", a: ["Sirius Black", "Sneverus Snape", "Tonks", "Albus Dumbledore"]},
+        { q: "Which house was Harry Potter sorted into?", 
+            a: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]},
+        { q: "What is the name of the Harry's Wizard School?", 
+            a: ["Hogwarts", "Beauxbatons", "Ilvermorny", "Durmstrang"]},
+        { q: "Who is Harry's godfather?", 
+            a: ["Sirius Black", "Sneverus Snape", "Tonks", "Albus Dumbledore"]},
     ];
 
-    function nextQuestion(){
+    function nextQuestion() {
+
         //If there are more questions, show next one
         if (questionIndex <= (questions.length - 1)) {
-            $("#question").textContent = questions[questionIndex].q;
+
+            $("#question").html(questions[questionIndex].q);
             console.log(questions[questionIndex].q);
+
         } else {
+
             $(".answerBox").textContent = "Game Over!";
+
         }
     }
-    //Hides Start button
+
+    function run() { //full timer function
+        
+        //resets timer to stop extra instances
+        clearInterval(interCount);
+        
+        //time decreasing each second
+        interCount = setInterval(decrement, 1000);
+
+    }
+
+    function decrement() { //decrement function used in run function above
+        
+        //decrease var timer by 1
+        count--;
+
+        //Countdown Timer in webpage
+        $(".timerBox").html("Time Remaining: " + count + " seconds");
+
+        //what happens when time and var timer reaches zero
+        if (count === 0 ) {
+            stop(); 
+            alert("Time's Up!");
+        }
+
+    }
+
+    function stop(){ //stops timer
+        clearInterval(interCount);
+    }
+
+    //Initializes Game
     $("#start").click(function(){
-        $(this).hide();
         
-        function run() {
-            clearInterval(interCount);
-            interCount = setInterval(decrement, 1000);
-        }
-        function decrement() {
-            count--;
-
-            //Countdown Timer
-            $(".timerBox").html("Time Remaining: " + count + " seconds");
-
-            if (count === 0 ) {
-                stop();
-                alert("Time's Up!");
-            }
-        }
-        function stop(){
-            clearInterval(interCount);
-        }
-
+        $(this).hide(); //hides start button after click
+              
+        run(); // call to start timer & all associated functions
         
-        run();
-        
-        nextQuestion();
+        nextQuestion(); //shows question
         
     });
     
