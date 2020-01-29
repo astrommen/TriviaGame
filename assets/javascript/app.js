a965c318d5194bac5adef619b28590dffcb97a0a
Aja Strommen <9466667+astrommen@users.noreply.github.com>
2020-01-28 22:22:08 -0500

    changed gifs to img, final cleanup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,17 +11,32 @@ $(document).ready(function() {
     //The array of questions for out game
     var questions = [
         { q: "Which house was Harry Potter sorted into?", 
-            a: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"],
-            correct: "Gryffindor"},
+            a: ["Ravenclaw", "Gryffindor", "Hufflepuff", "Slytherin"],
+            correct: "Gryffindor", 
+            img: "https://www.bing.com/th/id/OIP.T74hl6yd9lFGbTRZvgxHbgHaJl?w=170&h=220&c=7&o=5&pid=1.7"},
         { q: "What is the name of the Harry's Wizard School?", 
-            a: ["Hogwarts", "Beauxbatons", "Ilvermorny", "Durmstrang"],
-            correct: "Hogwarts"},
+            a: ["Beauxbatons", "Hogwarts", "Ilvermorny", "Durmstrang"],
+            correct: "Hogwarts",
+            img: "http://cdn4.tillthemoneyrunsout.com/wp-content/uploads/2015/11/Hogwarts-Castle-as-Seen-from-Dragon-Challenge-Line-Visiting-Harry-Potter-World-Orlando1-980x653.jpg"},
         { q: "Who is Harry's godfather?", 
-            a: ["Sirius Black", "Sneverus Snape", "Tonks", "Albus Dumbledore"],
-            correct: "Sirius Black"},
+            a: ["Sirius Black", "Severus Snape", "Michael Corleone", "Albus Dumbledore"],
+            correct: "Sirius Black",
+            img: "https://www.bing.com/th/id/OIP.ox1jAApUxSRAGc4jTocO-QHaJk?w=201&h=261&c=7&o=5&pid=1.7"},
+        {q: "What position did Harry play in Quidditch?",
+            a: ["Beater", "Keeper", "Seeker", "Chaser"],
+            correct: "Seeker",
+            img: "https://www.bing.com/th/id/OIP.WsXEFd7WhIHA4sL_80z1YwHaGl?w=202&h=176&c=7&o=5&pid=1.7"},
+        {q: "What Hogwarts House did everyone hate?",
+            a: ["Hufflepuff", "Ravenclaw", "Gryffindor", "Slytherin"],
+            correct: "Slytherin",
+            img: "https://www.bing.com/th/id/OIP.hY9yWy3Z9lR8HlzsLrn27wHaHa?w=192&h=186&c=7&o=5&pid=1.7"},
+        {q: "Who was Harry Potter's nemesis?",
+            a: ["Grindlewald", "Voldemort", "Knights of Nee", "Beedle the Bard"],
+            correct: "Voldemort",
+            img: "https://www.bing.com/th/id/OIP.0QQ_4FXNxGIWQoL4v_l3XgHaEK?w=251&h=160&c=7&o=5&pid=1.7"}
     ];
 
-    function nextQuestion() {
+      function nextQuestion() {
         
         //If there are more questions, show next one
         if (questionIndex <= (questions.length - 1)) {
@@ -71,24 +86,32 @@ $(document).ready(function() {
         var x = $(this).attr("data-name"); //variable to grab btn value
         console.log(x);
         
+        var ansImg = $("<img src=" + questions[questionIndex].img + "/>");
+        
         //comparison of user choice to correct answer
         if (x === questions[questionIndex].correct) {
             
             correctScore++; //if correct, adds one to the score
-
+            $("#question").text("Correctomundo!");
+   
         } else {
-
+            
             wrongScore++; //if wrong, adds one to wrong score
-
+            $("#question").text("Nope-ski!");
         }
         
+        stop();
+
         questionIndex++; //either way adds one to question index for nextQues function below
-        nextQuestion();               
+
+        $(".answerBox").html(ansImg);
+
+        setTimeout(nextQuestion, 3000);               
     });
     
     //Beginning of Clock Functions
     function run() { //full timer function
-        count = 30; //reset html count to 30 each time
+        count = 31; //reset html count to 30 each time
         
         //resets timer to stop extra instances
         clearInterval(interCount);
@@ -110,8 +133,11 @@ $(document).ready(function() {
         if (count === 0 ) {
 
             stop(); //stops the clock
+
             alert("Time's Up!");
+            timedOutScore++; //adds one to missed score
             questionIndex++; //adds one to question index for nextQues function below
+            
             nextQuestion();
         }
     }
